cb7f7c706fb3366302ec5a255006634df14173d7
DOREENKDAVID <doreenkinya153@gmail.com>
2024-01-03 11:23:06 -0800

    First-In First-Out caching algorithim

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -20,17 +20,16 @@ class FIFOCache(BaseCaching):
         """
         if key is None or item is None:
             return
+        self.cache_data[key] = item
 
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            # get the oldest key using an iterator and next
             oldest_key, _ = self.cache_data.popitem(False)
             print("DISCARD:", oldest_key)
 
-        self.cache_data[key] = item
 
     def get(self, item):
         """Get an item by key
         """
         if item is None or item not in self.cache_data:
             return None
-        return self.cache_data.get(key, None)
+        return self.cache_data.get(key)
