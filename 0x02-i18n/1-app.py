cf56376b9af985cf234829934991bf67a2127c7f
DOREENKDAVID <doreenkinya153@gmail.com>
2024-01-12 13:16:30 -0800

    i18n

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python3
-"""Basic Babel setup"""
-
+"""
+A Basic flask application
+"""
 from flask import Flask
 from flask import render_template
 from flask_babel import Babel
 
 
 class Config(object):
-    """Configuration class"""
+    """
+    Application configuration class
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-    # instanciate the app object
-    app = Flask(__name__)
-    app.config.from_object(Config)
 
-    # wrap app with babel
-    babel = Babel(app)
+# Instantiate the application object
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    @app.route('/', strict_slashes=False)
-    def index() -> str:
-        """
-        Renders 1-index. html template
-        """
-        return render_template('1-index.html')
+# Wrap the application with Babel
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
